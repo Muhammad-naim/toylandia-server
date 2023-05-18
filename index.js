eba241707df22dcd708d926a2925e0fd1be6d0db
naim <naimulislam0927@gmail.com>
2023-05-18 08:46:52 +0600

    modified toysData

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toysCollection = client.db('toylandiaDB').collection('toys');
+    app.get('/toys', async (req, res) => {
+      const cursor = toysCollection.find()
+      const toys = await cursor.toArray()
+      res.send(toys)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
